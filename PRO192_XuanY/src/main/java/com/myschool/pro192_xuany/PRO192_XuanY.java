6a3fb9f9d0a4056ac26c30523cc5d44f6848b121
Your name <your_email@example.com>
2023-09-21 14:52:11 +0700

    XuanY

---
+++ PRO192_XuanY/src/main/java/com/myschool/pro192_xuany/PRO192_XuanY.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myschool.pro192_xuany;
+
+/**
+ *
+ * @author Xuan Y
+ */
+public class PRO192_XuanY {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
